585fd6f233c8f0a5eb963fc12a4e1240c9168223
Tiến Phát <Tienphat040322@gmail.com>
2026-03-04 09:23:07 +0700

    hit vfx

--- Assets/ThachSanhGeneral/Phat/Scripts/ChanTinhBossController.cs
+++ Assets/ThachSanhGeneral/Phat/Scripts/ChanTinhBossController.cs
@@ -335,6 +335,12 @@ public class ChanTinhBossController : MonoBehaviour
             {
                 playerHealth.TakeDamage(attackDamage);
                 Debug.Log($"Chan Tinh Boss dealt {attackDamage} damage to {hitCollider.name}!");
+
+                // Spawn hit VFX at player center (bounds.center = giữa thân player)
+                if (vfxController != null)
+                {
+                    vfxController.PlayHitVfx(hitCollider.bounds.center);
+                }
             }
 
             // Can add other components if the player has multiple health scripts
